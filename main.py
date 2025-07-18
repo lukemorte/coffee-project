e68361d74968e80c05ebb01c48ae993af18dee52
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-07-18 12:44:01 +0200

    Přidány počáteční TODO úkoly pro funkcionalitu aplikace kávovaru.

--- main.py
+++ main.py
@@ -4,4 +4,45 @@
 # code
 
 
-print("Coffee Project")
+# TODO: 1. program se zeptá: Co si dáte? (espreso, latte, cappucino)
+# a) zadej, co chceš vyrobit
+# b) tento dotaz by se měl objevit pokaždé, když je akce dokončena (nápoj dokončen,
+# nebo pokud např. nebyl dostatek zdrojů)
+
+
+# TODO: 2. Vypni program skrytým příkazem "off"
+
+
+# TODO: 3.Zobraz stav
+# a) pokud je zadán příkaz "report", zobrazí se následující report:
+# Water: 100ml
+# Milk: 50ml
+# Coffee: 76g
+# Money: $2.5
+
+
+# TODO: 4. Kontrola, jestli je dostatek zdrojů
+# a) když si uživatel vybere nápoj, program zjistí, jestli je dostatek zdrojů
+# b) pokud např. latte potřebuje 200ml vody a je k dispozici jen 100ml, tak to musí zahlásit, že není dostatek vody
+# c) to samé nastane u ostatních zdrojů
+
+
+# TODO: 5. Proces placení mincemi
+# a) pokud je dostatek zdrojů na vybraný nápoj, program požádá o platbu mincemi (kolik quarters, kolik dimes,
+# kolik nickles a kolik pennies)
+# b) quarter = $0.25, dime = $0.10, nickle = $0.05, penny = $0.01
+# c) zkalkuluj mince, které byly vloženy
+
+
+# TODO: 6. Kontrola, jestli byla transakce úspěšná?
+# a) zkontroluj, jestli uživatel vložil dostatek mincí. Např. pokud latte stojí $2.50 a uživatel vložil pouze
+# např. $0.52 zahlásí to, že nebyla vložena dostatečná částka a peníze jsou vráceny a vrátí se to zpět na bod 1
+# b) pokud uživatel vložil dostatek, do mašiny bude vložena přesná částka za nápoj (tzn. projeví se při příštím
+# příkazu report)
+# c) pokud uživatel vložil příliš mnoho, stroj vrátí zbytek mincí od největších po nejmenší
+
+
+# TODO: 7. Vytvoř nápoj
+# a) Pokud zdroje na nápoj jsou k dispozici a transakce dopadla dobře, "vytvoří" se nápoj a zdroje se sníží o 
+# zdroje potřebné k výrobě
+# b) jakmile je nápoj hotov, vypiš hlášku, zde je váš ........., užijde si jej.
