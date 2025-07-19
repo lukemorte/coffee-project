87177fe6ebbfc4e89a45891b5729bf718e0f479a
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-07-19 09:03:43 +0200

    Refactor order handling and resource checks in coffee machine program

--- main.py
+++ main.py
@@ -34,11 +34,6 @@ def reset_coins():
     }
 
 
-# DONE: 1. program se zeptá: Co si dáte? (espreso, latte, cappucino)
-# a) zadej, co chceš vyrobit
-# b) tento dotaz by se měl objevit pokaždé, když je akce dokončena (nápoj dokončen,
-# nebo pokud např. nebyl dostatek zdrojů)
-
 def ask_for_order():
     options = " | ".join([f"{n} (${menu[n]["price"]:.2f})" for n in menu.keys()])
     order = input(f"The machine offers: \n\n{options}\n\nWhat would you like to have? : ")
@@ -53,17 +48,6 @@ def ask_for_order():
     return order
 
 
-# DONE: 2. Vypni program skrytým příkazem "off"
-
-
-# DONE: 3. Zobraz stav
-# a) pokud je zadán příkaz "report", zobrazí se následující report:
-# Water: 100ml
-# Milk: 50ml
-# Coffee: 76g
-# Money: $2.5
-
-
 def show_report():
     print("Current machine's resources:\n")
     for item in machine:
@@ -74,13 +58,6 @@ def show_report():
             print(f"{item.capitalize()}: {value}")
 
     print("\n")
-
-
-# DONE: 4. Kontrola, jestli je dostatek zdrojů
-# a) když si uživatel vybere nápoj, program zjistí, jestli je dostatek zdrojů
-# b) pokud např. latte potřebuje 200ml vody a je k dispozici jen 100ml, tak to musí zahlásit, že není dostatek vody
-# c) to samé nastane u ostatních zdrojů.
-# d) pokud není dostatek zdrojů, vrací se na bod 1
 
 
 def check_for_resources(order):
@@ -100,13 +77,6 @@ def check_for_resources(order):
 
     print('\n')
     return is_order_possible
-
-
-# DONE: 5. Proces placení mincemi
-# a) pokud je dostatek zdrojů na vybraný nápoj, program požádá o platbu mincemi (kolik quarters, kolik dimes,
-# kolik nickles a kolik pennies)
-# b) quarter = $0.25, dime = $0.10, nickle = $0.05, penny = $0.01
-# c) zkalkuluj mince, které byly vloženy
 
 
 def get_input_number(s):
@@ -158,7 +128,7 @@ def pay_for_order(coins, p_order):
     print(f"Your order is: {p_order.capitalize()} (cost ${price:.2f})")
     print("Please, insert coins:\n")
 
-    for item in coins:        
+    for item in coins:
         coins[item] = get_input_number(f"Insert {item}: ")
         show_current_coins(coins)
 
@@ -178,19 +148,6 @@ def pay_for_order(coins, p_order):
     show_remain_coins(coins)
 
 
-# DONE: 6. Kontrola, jestli byla transakce úspěšná?
-# a) zkontroluj, jestli uživatel vložil dostatek mincí. Např. pokud latte stojí $2.50 a uživatel vložil pouze
-# např. $0.52 zahlásí to, že nebyla vložena dostatečná částka a peníze jsou vráceny a vrátí se to zpět na bod 1
-# b) pokud uživatel vložil dostatek, do mašiny bude vložena přesná částka za nápoj (tzn. projeví se při příštím
-# příkazu report)
-# c) pokud uživatel vložil příliš mnoho, stroj vrátí zbytek mincí od největších po nejmenší
-
-
-# DONE: 7. Vytvoř nápoj
-# a) Pokud zdroje na nápoj jsou k dispozici a transakce dopadla dobře, "vytvoří" se nápoj a zdroje se sníží o
-# zdroje potřebné k výrobě
-# b) jakmile je nápoj hotov, vypiš hlášku, zde je váš ........., užijde si jej.
-
 def consume_resources(order):
     machine['water'] -= menu[order]["ingredients"]["water"]
     machine['milk'] -= menu[order]["ingredients"]["milk"]
@@ -207,7 +164,7 @@ def create_product(order):
 def replenish_machine():
     clr()
     show_report()
-    print("You replenished the machine with resources.\nThe machine is full again.\n")        
+    print("You replenished the machine with resources.\nThe machine is full again.\n")
     global machine
     machine["water"] = 500
     machine["milk"] = 300
@@ -253,5 +210,5 @@ def main_app():
 
 # code
 
-
+# run program
 main_app()
